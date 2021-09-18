d9a146f6381783d9fe4abddc2f2339f54af1b058
GuillaumeGSO <GuillaumeGSO@users.noreply.github.com>
2021-09-18 17:42:25 +0800

    Separation between mocked data and company service

--- src/company-data/company-data.service.ts
+++ src/company-data/company-data.service.ts
@@ -1,123 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { User, Company, ChannelEnum } from './company-data.interface';
+import { getCompanyStub } from './company-data.stubs';
 /**
  * Mocked Company data service
  */
 @Injectable()
 export class CompanyDataService {
-  private users4Monster: User[] = [
-    //For MonsterINC company
-    this.generateUser('10', 'Bob', 'WARWOSKY', 'bob@monster.inc', [
-      ChannelEnum.EMAIL,
-      ChannelEnum.UI,
-    ]),
-    this.generateUser('11', 'Fred', 'WARWOSKY', 'fred@monster.inc', [
-      ChannelEnum.EMAIL,
-    ]),
-    this.generateUser('12', 'Tom', 'WARWOSKY', 'tom@monster.inc', [
-      ChannelEnum.UI,
-    ]),
-    this.generateUser('13', 'John', 'WARWOSKY', 'john@monster.inc', []),
-  ];
-  private users4Dalton: User[] = [
-    //For DALTON sarl company
-    this.generateUser('20', 'Joe', 'DALTON', 'joe@dalton.com', [
-      ChannelEnum.EMAIL,
-      ChannelEnum.UI,
-    ]),
-    this.generateUser('21', 'Jack', 'DALTON', 'jack@dalton.com', [
-      ChannelEnum.EMAIL,
-    ]),
-    this.generateUser('22', 'William', 'DALTON', 'william@dalton.com', [
-      ChannelEnum.UI,
-    ]),
-    this.generateUser('23', 'Avrel', 'DALTON', 'avrel@dalton.com', []),
-  ];
-  private users4Simpsons: User[] = [
-    //For The Simpson's company
-    this.generateUser('30', 'Bart', 'SIMPSON', 'bart@simpsons.com', [
-      ChannelEnum.EMAIL,
-      ChannelEnum.UI,
-    ]),
-    this.generateUser('31', 'Homer', 'SIMPSON', 'homer@simpsons.com', [
-      ChannelEnum.EMAIL,
-    ]),
-    this.generateUser('32', 'Marge', 'SIMPSON', 'Marge@simpsons.com', [
-      ChannelEnum.UI,
-    ]),
-    this.generateUser('33', 'Lisa', 'SIMPSON', 'lisa@simpsons.com', []),
-  ];
-
-  private users4Heroes: User[] = [
-    //For The Simpson's company
-    this.generateUser('40', 'Captain', 'AMERICA', 'captain@heroes.com', [
-      ChannelEnum.EMAIL,
-      ChannelEnum.UI,
-    ]),
-    this.generateUser('41', 'Peter', 'SPIDERMAN', 'peter@heroes.com', [
-      ChannelEnum.EMAIL,
-    ]),
-    this.generateUser('42', 'Clark', 'SUPERMAN', 'clark@heroes.com', [
-      ChannelEnum.UI,
-    ]),
-    this.generateUser('43', 'Cat', 'WOMAN', 'cat@heroes.com', []),
-  ];
-
-  private companies: Company[] = [
-    this.generateCompany(
-      '000',
-      'EMPTY',
-      [],
-      [ChannelEnum.EMAIL, ChannelEnum.UI],
-    ),
-
-    this.generateCompany('100', 'MONSTER Inc', this.users4Monster, [
-      ChannelEnum.EMAIL,
-      ChannelEnum.UI,
-    ]),
-
-    this.generateCompany('200', 'DALTON Sarl', this.users4Dalton, [
-      ChannelEnum.EMAIL,
-    ]),
-
-    this.generateCompany('300', "THE SIMPSON'S", this.users4Simpsons, [
-      ChannelEnum.UI,
-    ]),
-
-    this.generateCompany('400', "MARVEL", this.users4Heroes, []),
-  ];
-
+  
   getCompanyById(companyId: string): Company {
-    return this.companies.find((cp) => cp.companyId === companyId);
+    return getCompanyStub().find((cp) => cp.companyId === companyId);
   }
 
-  private generateCompany(
-    companyId: string,
-    companyName: string,
-    users: User[],
-    channels: ChannelEnum[],
-  ): Company {
-    return {
-      companyId: companyId,
-      companyName: companyName,
-      users: users,
-      subscribedChannels: channels,
-    } as Company;
-  }
-
-  private generateUser(
-    userId: string,
-    firstName: string,
-    lastName: string,
-    email: string,
-    channelEnums: ChannelEnum[],
-  ): User {
-    return {
-      userId: userId,
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      subscribedChannels: channelEnums,
-    } as User;
-  }
 }
---
+++ src/company-data/company-data.stubs.ts
@@ -0,0 +1,124 @@
+import { User, Company, ChannelEnum } from './company-data.interface'
+
+/** This generate a dataset of companies and users to
+/* be used during unit testing and mocked service
+**/
+export const getCompanyStub = (): Company[] => {
+    return companies;
+}
+
+const users4Monster: User[] = [
+    //For MonsterINC company
+    generateUser('10', 'Bob', 'WARWOSKY', 'bob@monster.inc', [
+        ChannelEnum.EMAIL,
+        ChannelEnum.UI,
+    ]),
+    generateUser('11', 'Fred', 'WARWOSKY', 'fred@monster.inc', [
+        ChannelEnum.EMAIL,
+    ]),
+    generateUser('12', 'Tom', 'WARWOSKY', 'tom@monster.inc', [
+        ChannelEnum.UI,
+    ]),
+    generateUser('13', 'John', 'WARWOSKY', 'john@monster.inc', []),
+];
+
+const users4Dalton: User[] = [
+    //For DALTON sarl company
+    generateUser('20', 'Joe', 'DALTON', 'joe@dalton.com', [
+        ChannelEnum.EMAIL,
+        ChannelEnum.UI,
+    ]),
+    generateUser('21', 'Jack', 'DALTON', 'jack@dalton.com', [
+        ChannelEnum.EMAIL,
+    ]),
+    generateUser('22', 'William', 'DALTON', 'william@dalton.com', [
+        ChannelEnum.UI,
+    ]),
+    generateUser('23', 'Avrel', 'DALTON', 'avrel@dalton.com', []),
+];
+
+const users4Simpsons: User[] = [
+    //For The Simpson's company
+    generateUser('30', 'Bart', 'SIMPSON', 'bart@simpsons.com', [
+        ChannelEnum.EMAIL,
+        ChannelEnum.UI,
+    ]),
+    generateUser('31', 'Homer', 'SIMPSON', 'homer@simpsons.com', [
+        ChannelEnum.EMAIL,
+    ]),
+    generateUser('32', 'Marge', 'SIMPSON', 'Marge@simpsons.com', [
+        ChannelEnum.UI,
+    ]),
+    generateUser('33', 'Lisa', 'SIMPSON', 'lisa@simpsons.com', []),
+];
+
+const users4Heroes: User[] = [
+    //For The Simpson's company
+    generateUser('40', 'Captain', 'AMERICA', 'captain@heroes.com', [
+        ChannelEnum.EMAIL,
+        ChannelEnum.UI,
+    ]),
+    generateUser('41', 'Peter', 'SPIDERMAN', 'peter@heroes.com', [
+        ChannelEnum.EMAIL,
+    ]),
+    generateUser('42', 'Clark', 'SUPERMAN', 'clark@heroes.com', [
+        ChannelEnum.UI,
+    ]),
+    generateUser('43', 'Cat', 'WOMAN', 'cat@heroes.com', []),
+];
+
+const companies: Company[] = [
+    generateCompany(
+        '000',
+        'EMPTY',
+        [],
+        [ChannelEnum.EMAIL, ChannelEnum.UI],
+    ),
+
+    generateCompany('100', 'MONSTER Inc', users4Monster, [
+        ChannelEnum.EMAIL,
+        ChannelEnum.UI,
+    ]),
+
+    generateCompany('200', 'DALTON Sarl', users4Dalton, [
+        ChannelEnum.EMAIL,
+    ]),
+
+    generateCompany('300', "THE SIMPSON'S", users4Simpsons, [
+        ChannelEnum.UI,
+    ]),
+
+    generateCompany('400', "MARVEL", users4Heroes, []),
+];
+
+function generateCompany(
+    companyId: string,
+    companyName: string,
+    users: User[],
+    channels: ChannelEnum[],
+): Company {
+    return {
+        companyId: companyId,
+        companyName: companyName,
+        users: users,
+        subscribedChannels: channels,
+    } as Company;
+}
+
+function generateUser(
+    userId: string,
+    firstName: string,
+    lastName: string,
+    email: string,
+    channelEnums: ChannelEnum[],
+): User {
+    return {
+        userId: userId,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        subscribedChannels: channelEnums,
+    } as User;
+}
+
+
