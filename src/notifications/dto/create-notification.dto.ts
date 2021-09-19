61f177879d34d2c19eb9c862e97bf0fcb06c9e96
GuillaumeGSO <GuillaumeGSO@users.noreply.github.com>
2021-09-19 16:06:17 +0800

    Better Dto validation

--- src/notifications/dto/create-notification.dto.ts
+++ src/notifications/dto/create-notification.dto.ts
@@ -1,13 +1,18 @@
-import { IsIn, IsNotEmpty } from 'class-validator';
+import { IsIn, IsNotEmpty, IsString } from 'class-validator';
 
-const notificationTypes =['leave-balance-reminder', 'monthly-payslip', 'happy-birthday'];
+const notificationTypes = ['leave-balance-reminder', 'monthly-payslip', 'happy-birthday'];
 
 export class CreateNotificationDto {
+
   @IsNotEmpty()
+  @IsString()
   readonly companyId: string;
 
+  @IsString()
+  @IsNotEmpty()
   readonly userId: string;
 
+  @IsString()
   @IsNotEmpty()
   @IsIn(notificationTypes)
   readonly notificationType: string;
